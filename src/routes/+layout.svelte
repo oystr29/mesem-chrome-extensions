ebbac96629b85214c79d99b96acdcc2f101d6895
oktoala <okta.yoga.syah@gmail.com>
2023-06-18 15:18:06 +0800

    Add trackteer icon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,7 @@
 	// @ts-ignore
 	import { tooltip } from '@svelte-plugins/tooltips';
 	import '@fontsource/roboto';
+  import TrtIcon from '../../static/trbtn-icon.png'
 	import type { PageData } from './$types';
 	import '../app.css';
 	import Alert from '../components/Alert.svelte';
@@ -43,13 +44,13 @@
 <div class="min-w-[600px] roboto">
 	<Alert />
 	<Sidebar />
-	<div class="flex bg-white dark:bg-gray-900 text-black dark:text-white">
+	<div class="flex bg-gray-50 dark:bg-gray-900 text-black dark:text-white">
 		<div class="ml-20 w-full">
 			<div
 				class="shadow-lg bg-white dark:bg-gray-950 text-black dark:text-white px-3 py-2 sticky top-0 flex items-center justify-between"
 			>
 				<h2 class="text-xl font-semibold">{data.slug ? mesem[data.slug].text : 'Latest'}</h2>
-				<div class="flex items-center pr-10">
+				<div class="flex items-center pr-2">
 					<a
 						use:tooltip={{
 							arrow: false,
@@ -86,6 +87,9 @@
 							<SunIcon />
 						{/if}
 					</button>
+          <a target="_blank" rel="noopener noreferrer" href="https://trakteer.id/oktavian_yoga/tip" class="h-6 ml-3">
+            <img class="h-6" src={TrtIcon} alt="Traktir Icon" />
+          </a>
 				</div>
 			</div>
 			<div class="min-h-screen mx-5 h-auto">
